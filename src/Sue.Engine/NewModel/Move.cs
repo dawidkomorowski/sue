88551edfe7f16eba7c988df5fe69220780489ad7
Dawid Komorowski <dawidkomorowski@outlook.com>
2024-05-23 19:35:52 +0200

    Create new move model

---
+++ src/Sue.Engine/NewModel/Move.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Sue.Engine.Model.Chessboard;
+
+namespace Sue.Engine.NewModel;
+
+internal struct Move : IEquatable<Move>
+{
+    public Move(Position from, Position to, Promotion promotion = Promotion.None)
+    {
+        From = from;
+        To = to;
+        Promotion = promotion;
+    }
+
+    public Position From { get; }
+    public Position To { get; }
+    public Promotion Promotion { get; }
+
+    public override string ToString() => $"{nameof(From)}: {From}, {nameof(To)}: {To}, {nameof(Promotion)}: {Promotion}";
+
+    public bool Equals(Move other) => From.Equals(other.From) && To.Equals(other.To) && Promotion == other.Promotion;
+    public override bool Equals(object? obj) => obj is Move other && Equals(other);
+    public override int GetHashCode() => HashCode.Combine(From, To, (int)Promotion);
+    public static bool operator ==(Move left, Move right) => left.Equals(right);
+    public static bool operator !=(Move left, Move right) => !left.Equals(right);
+}
+
+internal struct Position : IEquatable<Position>
+{
+    public Position(File file, Rank rank)
+    {
+        File = file;
+        Rank = rank;
+    }
+
+    public File File { get; }
+    public Rank Rank { get; }
+
+    public override string ToString() => $"{nameof(File)}: {File}, {nameof(Rank)}: {Rank}";
+
+    public bool Equals(Position other) => File == other.File && Rank == other.Rank;
+    public override bool Equals(object? obj) => obj is Position other && Equals(other);
+    public override int GetHashCode() => HashCode.Combine((int)File, (int)Rank);
+    public static bool operator ==(Position left, Position right) => left.Equals(right);
+    public static bool operator !=(Position left, Position right) => !left.Equals(right);
+}
+
+internal enum Promotion
+{
+    None,
+    Queen,
+    Rook,
+    Bishop,
+    Knight
+}
---
+++ test/Sue.Engine.UnitTests/NewModel/MoveTests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using Sue.Engine.Model.Chessboard;
+using Sue.Engine.NewModel;
+
+namespace Sue.Engine.UnitTests.NewModel;
+
+[TestFixture]
+[TestOf(typeof(Move))]
+public class MoveTests
+{
+    [Test]
+    public void Constructor_ShouldCreateMoveWithoutPromotion()
+    {
+        // Arrange
+        var from = new Position(File.A, Rank.One);
+        var to = new Position(File.B, Rank.Two);
+
+        // Act
+        var actual = new Move(from, to);
+
+        // Assert
+        Assert.That(actual.From, Is.EqualTo(from));
+        Assert.That(actual.To, Is.EqualTo(to));
+        Assert.That(actual.Promotion, Is.EqualTo(Promotion.None));
+    }
+
+    [Test]
+    public void Constructor_ShouldCreateMoveWithPromotion()
+    {
+        // Arrange
+        var from = new Position(File.A, Rank.One);
+        var to = new Position(File.B, Rank.Two);
+        var promotion = Promotion.Queen;
+
+        // Act
+        var actual = new Move(from, to, promotion);
+
+        // Assert
+        Assert.That(actual.From, Is.EqualTo(from));
+        Assert.That(actual.To, Is.EqualTo(to));
+        Assert.That(actual.Promotion, Is.EqualTo(promotion));
+    }
+}
