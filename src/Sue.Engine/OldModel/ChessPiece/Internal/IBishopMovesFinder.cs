6557b0dde4c1ceb591db46440ff262d7d721f084
Dawid Komorowski <dawidkomorowski@outlook.com>
2024-06-07 17:58:22 +0200

    Remove legacy model

--- src/Sue.Engine/OldModel/ChessPiece/Internal/IBishopMovesFinder.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace Sue.Engine.OldModel.ChessPiece.Internal
-{
-    public interface IBishopMovesFinder
-    {
-        IEnumerable<IMove> FindMoves(IChessPiece chessPiece);
-    }
-}
