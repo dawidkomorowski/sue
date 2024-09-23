9f28ffa022ab71648008feba6f100faa6c058a73
Dawid Komorowski <dawidkomorowski@outlook.com>
2024-09-23 21:17:58 +0200

    Create engine instance before actual benchmark

--- benchmark/Sue.Engine.MicroBenchmark/Program.cs
+++ benchmark/Sue.Engine.MicroBenchmark/Program.cs
@@ -14,22 +14,28 @@ internal static class Program
 [MemoryDiagnoser]
 public class ChessEngineBenchmark
 {
-    [IterationCount(1)]
-    [Benchmark]
-    public void FindBestMove()
+    private ChessEngine _chessEngine = null!;
+
+    [IterationSetup]
+    public void IterationSetUp()
     {
         var engineSettings = new EngineSettings
         {
             RandomSeed = 0
         };
 
-        var chessEngine = new ChessEngine(engineSettings);
+        _chessEngine = new ChessEngine(engineSettings);
+    }
 
+    [IterationCount(1)]
+    [Benchmark]
+    public void FindBestMove()
+    {
         var searchSettings = new SearchSettings
         {
             FixedDepth = 7
         };
 
-        chessEngine.FindBestMove("1rbr2k1/5pp1/1b3nnp/1pp1pN2/4P3/2P1BN1P/1PB2PP1/R3R1K1 b - - 11 25", "", searchSettings);
+        _chessEngine.FindBestMove("1rbr2k1/5pp1/1b3nnp/1pp1pN2/4P3/2P1BN1P/1PB2PP1/R3R1K1 b - - 11 25", "", searchSettings);
     }
 }
